b564c300d77b9e6b30192d2076ac14b62bc9f1dc
xatiko540 <xatiko541@gmail.com>
2022-08-05 22:08:23 +0300

    rtmp_publisher_Youinroll

--- test/rtmp_publisher_test.dart
+++ test/rtmp_publisher_test.dart
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:rtmp_publisher/new/camera.dart';
-import 'package:rtmp_publisher/new/src/camera_testing.dart';
-import 'package:rtmp_publisher/new/src/common/native_texture.dart';
+import 'rtmp_publisher_Youinroll/new/camera.dart';
+import 'rtmp_publisher_Youinroll/new/src/camera_testing.dart';
+import 'rtmp_publisher_Youinroll/new/src/common/native_texture.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
